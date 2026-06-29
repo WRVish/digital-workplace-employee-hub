33af2797acbfa7d5b86a1901297a0d99e9102ae0
Vishnu <wrvishnu@gmail.com>
2026-06-29 20:25:16 +0800

    Remove temporary scaffolding and linting scripts

--- digital-workplace-employee-hub/fix-icons.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const file = path.join(__dirname, 'src/components/Icons.tsx');
-let content = fs.readFileSync(file, 'utf8');
-content = content.replace(/([a-z]+): \(\) =>/g, (match, p1) => {
-  return p1.charAt(0).toUpperCase() + p1.slice(1) + ': () =>';
-});
-fs.writeFileSync(file, content);
